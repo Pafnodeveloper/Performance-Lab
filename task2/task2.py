443532044b2dca511e877d1d981f4c67db5846c7
Pafnodeveloper <51054260+Pafnodeveloper@users.noreply.github.com>
2021-03-09 11:27:41 +0300

    Add files via upload

---
+++ task2/task2.py
@@ -0,0 +1,19 @@
+# import matplotlib as mtl
+# import matplotlib.pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
+# import numpy as np
+#
+# fig = plt.figure(figsize=(10,10))
+# ax = fig.add_subplot(111, projection = '3d')
+#
+# u = np.linspace(10, 10 * np.pi, 100)
+# v = np.linspace(10, 5 * np.pi, 100)
+#
+# x = 10 * np.outer(np.cos(u), np.sin(v))
+# y = 10 * np.outer(np.sin(u), np.sin(v))
+# z = 10 * np.outer(np.ones(np.size(u)), np.cos(v))
+# ax.plot_surface(x, y, z, rstride=4, cstride=4, color='b')
+#
+# plt.show()
+
+print("Не смог в математику")
